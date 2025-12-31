651fa77062138cdab6d93f2982c6ad88947bba7e
Akshita Singh <akshita1060.be24@chitkarauniversity.edu.in>
2025-12-31 22:16:57 +0530

    Create median_of_two_sorted_arrays.cpp

---
+++ array/median_of_two_sorted_arrays.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        for(int i=0;i<nums2.size();i++){
+            nums1.push_back(nums2[i]);
+        }
+        sort(nums1.begin(),nums1.end());
+        int result = nums1.size()/2;
+        if(nums1.size()%2!=0){
+            return nums1[result];
+        }
+        else{
+            return (nums1[result-1]+nums1[result])/2.0;
+        }
+    }
+};
